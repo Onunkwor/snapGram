0b1efb1f8b4dd7547094b73658aff1b41124a174
raphael <you@example.com>
2024-03-29 07:55:52 +0100

    fixed router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -28,8 +28,8 @@ const router = createBrowserRouter([
       { path: "/update-post/:id", element: <EditPost /> },
     ],
   },
-  { path: "sign-in", element: <SignInPage /> },
-  { path: "sign-up", element: <SignUpPage /> },
+  { path: "/sign-in", element: <SignInPage /> },
+  { path: "/sign-up", element: <SignUpPage /> },
 ]);
 
 const Router = () => {
