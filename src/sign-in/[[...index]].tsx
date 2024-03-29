f29c83078f1195b48c166b403f8c3b647cd97c8a
raphael <you@example.com>
2024-03-29 23:06:24 +0100

    centered the sign-in page

--- src/sign-in/[[...index]].tsx
+++ src/sign-in/[[...index]].tsx
@@ -1,7 +1,9 @@
 import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  <div className="flex justify-center items-center w-full h-screen">
+    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  </div>
 );
 
 export default SignInPage;
