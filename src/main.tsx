44244447c9aba8ac5c22ce2de6503dc32a332441
raphael <you@example.com>
2024-03-29 22:24:57 +0100

    opened pandoras box

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 import { ClerkProvider } from "@clerk/clerk-react";
+import React from "react";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
@@ -11,7 +12,9 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <QueryProvider>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </QueryProvider>
   </ClerkProvider>
 );
