a04e6d40530445b98b1dc440bb4857335985041c
raphael <you@example.com>
2024-04-15 12:59:47 +0100

    feat: Add loader until Auth loads

--- src/ProtectedRoutes/index.tsx
+++ src/ProtectedRoutes/index.tsx
@@ -1,8 +1,18 @@
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
+import Loader from "@/components/shared/Loader";
 const ProtectedRoutes = ({ children }: { children: ReactNode }) => {
   const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="loader">
+          <Loader />;
+        </div>
+      </div>
+    );
+  }
   if (!isSignedIn && isLoaded) {
     return <Navigate to="/sign-in" />;
   }
